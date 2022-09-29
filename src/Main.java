c8268f690bdca07626ca0e6a0b49fe2336ae0c8a
AniParTt1k <104137164+AniParTt1k@users.noreply.github.com>
2022-09-29 20:20:01 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("This task done by Лісняк Іван ПЗ-19-1/9 ");
         System.out.println("26.09.2022");
+        System.out.println("Зміни у браузері");
     }
 }

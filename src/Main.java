1c87de19c88ba21b1463f6cf0ef6c05c8ed4821c
Ivan <Ivanlesnyak55@gmail.com>
2022-09-29 20:18:01 +0300

    Main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] argv) {
+
+        System.out.println("Hello world");
+        System.out.println("This task done by Лісняк Іван ПЗ-19-1/9 ");
+    }
+}
